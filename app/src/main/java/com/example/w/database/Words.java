006d817abe2c5a75f719695f80f7330fa85214a6
w <1379853502@qq.com>
2016-09-09 14:32:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/w/database/Words.java
@@ -0,0 +1,19 @@
+package com.example.w.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by w on 2016/9/9.
+ */
+
+public class Words {
+    public Words() {
+    }
+
+    public static abstract class Word implements BaseColumns {
+        public static final String TABLE_NAME="words";
+        public static final String COLUMN_NAME_WORD="word";//列：单词
+        public static final String COLUMN_NAME_MEANING="meaning";//列：单词含义
+        public static final String COLUMN_NAME_SAMPLE="sample";//单词示例
+    }
+}
